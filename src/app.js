ad3f77d4788e7f20d0b342eba46e4ab756704f77
Raphael Batagini <raphabatagini@gmail.com>
2023-06-21 15:35:37 -0300

    fix: get contract route to only retrieve contracts that belong to client or contractor

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
+const {sequelize, Profile} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +8,18 @@ app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
+app.get('/contracts/:id',getProfile,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const {profile} = req
+    const contract = await Contract.findOne({
+      where: {
+        id,
+        [profile.type === 'client' ? 'ClientId' :  'ContractorId']: profile.id
+      }
+    })
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
