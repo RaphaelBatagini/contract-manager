4cd8b0abad81292239c6e9081e23ce9320c1e4d7
Raphael Batagini <raphabatagini@gmail.com>
2023-06-23 00:57:05 -0300

    refactor: replace hardcoded http codes

--- src/adapters/http/contracts-http-controller.js
+++ src/adapters/http/contracts-http-controller.js
@@ -2,6 +2,7 @@ const getContractFactory = require("../../application/use-cases/get-contract/fac
 const listContractFactory = require("../../application/use-cases/list-contracts/factory");
 const ContractNotFoundError = require("../../application/errors/contract-not-found-error");
 const UserCannotAccessContractError = require("../../application/errors/user-cannot-access-contract-error");
+const httpStatus = require("http-status");
 
 class ContractsHttpController {
   async index(request, response) {
@@ -13,7 +14,9 @@ class ContractsHttpController {
       const contracts = await listContracts.execute(profile.id);
       return response.json(contracts);
     } catch (error) {
-      return response.status(500).end("internal server error");
+      return response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .end("internal server error");
     }
   }
 
@@ -28,16 +31,16 @@ class ContractsHttpController {
       return response.json(contract);
     } catch (error) {
       if (error instanceof ContractNotFoundError) {
-        return response.status(404).end("contract not found");
+        return response.status(httpStatus.NOT_FOUND).end(error.message);
       }
 
       if (error instanceof UserCannotAccessContractError) {
-        return response.status(403).end("contract doesn't belong to the user");
+        return response.status(httpStatus.FORBIDDEN).end(error.message);
       }
 
-      throw error;
-
-      return response.status(500).end("internal server error");
+      return response
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .end("internal server error");
     }
   }
 }
